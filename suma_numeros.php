c73d8b4f5e1e95904a410ad24a370acb78ada0ef
pcarcal254 <pcarcal254@g.educaand.es>
2022-09-26 09:53:06 +0200

    v2.0 Calculadora Cientifica

--- suma_numeros.php
+++ suma_numeros.php
@@ -1,8 +1,16 @@
 <?php
-    echo "<h1>CALCULADORA CIENTIFICA</h1> <br>";
-    $a = $_GET['a'];
-    $b = $_GET['b'];
-    $c = $a + $b;
-    echo "<h1>$a + $b = $c</h1>";
+    if (isset($_GET['a']) and isset($_GET['b'])) {
+        if ($_GET['simbolo'] == "mas") {
+            $c = $_GET['a'] + $_GET['b'];
+            echo "<h1>RESULTADO: $c</h1>";
+        } else {
+            $c = $_GET['a'] - $_GET['b'];
+            echo "<h1>RESULTADO: $c</h1>";
+        }
+    } elseif (empty($_GET['a'])) {
+        echo "<h1>EL VALOR A ESTA VACIO</h1>";
+    } elseif (empty($_GET['b'])) {
+        echo "<h1>EL VALOR B ESTA VACIO</h1>";
+    }
 ?>
 
